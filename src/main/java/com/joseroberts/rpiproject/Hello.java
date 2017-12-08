7427c3e8c9a90fa79698e35cfc13325ed5d4c9f5
robertiroberts85@gmail.com <robertiroberts85@gmail.com>
2017-12-07 23:23:25 -0500

    This is the Maven Jersey Tomcat project working. yay.

---
+++ src/main/java/com/joseroberts/rpiproject/Hello.java
@@ -0,0 +1,33 @@
+package com.joseroberts.rpiproject;
+
+import javax.servlet.http.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import java.io.IOException;
+@Path("/hello")
+public class Hello {
+
+    @GET
+    public Response getGenMsg() {
+
+        String output = "Jersey say : " + "General shit";
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+    @GET
+    @Path("/{param}")
+    public Response getMsg(@PathParam("param") String msg) {
+
+        String output = "Jersey say what the  : " + msg;
+
+        return Response.status(200).entity(output).build();
+
+    }
+
+}
