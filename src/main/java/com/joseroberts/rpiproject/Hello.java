24c08bb704b4bed74fa1e0404e3ebf1f35ac2e6e
robertiroberts85@gmail.com <robertiroberts85@gmail.com>
2017-12-09 23:47:40 -0500

    Added the ability to call database created. Need to create a "rpiproject" database and an "users" collections for this example to work. Ver 3

--- src/main/java/com/joseroberts/rpiproject/Hello.java
+++ src/main/java/com/joseroberts/rpiproject/Hello.java
@@ -1,19 +1,12 @@
 package com.joseroberts.rpiproject;
 
 import com.mongodb.MongoClient;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 import org.bson.Document;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
 @Path("/hello")
 
@@ -21,42 +14,34 @@ public class Hello {
 
     @GET
     public Response getGenMsg() {
-
-        String output = "Jersey say : " + "Bitcoin is the shit. You should invest NOW!!!";
-
-        return Response.status(200).entity(output).build();
-
+        String output = "Jersey say : " + "Bitcoin is the shit. You should invest NOW!!! \n\n ";
+        String dbOutput = getDatabase("rpiproject");
+        return Response.status(200).entity(output +"\n MongoDB has the following...   --->>> \n" + dbOutput).build();
     }
 
     @GET
     @Path("/{param}")
     public Response getMsg(@PathParam("param") String msg) {
-
-        String output = "Jersey say what the  : " + msg;
+        String output = "Jersey say  : " + msg;
         String dbOutput = getDatabase("rpiproject");
-
         return Response.status(200).entity(output +"\n Database has tge shitttt \n" + dbOutput).build();
 
     }
 
-    public String getDatabase(String dbName){
-
+    private String getDatabase(String dbName){
         MongoClient mongoClient = new MongoClient();
         MongoDatabase mongoDatabase = mongoClient.getDatabase(dbName);
-        MongoCollection collection = mongoDatabase.getCollection("users");
 
-
-        MongoCursor<Document> cursor = collection.find().iterator();
+        ListCollectionsIterable<Document> listCollection = mongoDatabase.listCollections();
+        MongoCursor<Document> cursor = listCollection.iterator();
         String output = "";
         try {
             while (cursor.hasNext()) {
                 output += cursor.next().toJson();
-                System.out.println(output);
             }
         } finally {
             cursor.close();
         }
         return output;
     }
-
 }
